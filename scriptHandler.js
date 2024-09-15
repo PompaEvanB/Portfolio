b18cb9e4649d822bc8485533987822304430ce78
Evan Pompa <55860113+PompaEvanB@users.noreply.github.com>
2024-09-15 13:01:58 -0700

    Added Js for project switching
    Made it so when you switch projects, the other projects get hidden. When you go to the qualifications tab, all projects get hidden and the container div gets closed

--- scriptHandler.js
+++ scriptHandler.js
@@ -1,20 +1,59 @@
+function HideProjects(){
+    $("#Evocation").hide();
+    $("#Sledge").hide();
+    $("#BitllutHill").hide();
+    $("#TimeLine").hide();
+    $("#YoureFired").hide();
+ }
+
 $(function(){
     // jQuery methods go here...
+
+    // Set these to hidden by default...
     $("#QualificationsDiv").hide();
     $("#ProjectsDiv").hide();
-    $("#Evocation").hide();
+    HideProjects(); // hides projects on start NOT by default
 
-
-
+    // Shows qualifications, hides all projects and project container
     $("#QualificationsButton").click(function(){
         $("#QualificationsDiv").show();
         $("#ProjectsDiv").hide();
+        HideProjects();
     });
+
+    // Present the option to look at all projects
     $("#ProjectsButton").click(function(){
         $("#ProjectsDiv").show();
         $("#QualificationsDiv").hide();
     });
+
+    // When Evocation is clicked. Hide other projects and show itself
     $("#EvocationButton").click(function(){
+        HideProjects();
         $("#Evocation").show();
+    });
+
+    // When Sledge is clicked. Hide other projects and show itself
+    $("#SledgeButton").click(function(){
+        HideProjects();
+        $("#Sledge").show();
+    });
+
+    // When BitllutHill is clicked. Hide other projects and show itself
+    $("#BitllutHillButton").click(function(){
+        HideProjects();
+        $("#BitllutHill").show();
+    });
+
+    // When TimeLine is clicked. Hide other projects and show itself
+    $("#TimeLineButton").click(function(){
+        HideProjects();
+        $("#TimeLine").show();
+    });
+
+    // When Youre Fired is clicked. Hide other projects and show itself
+    $("#YoureFiredButton").click(function(){
+        HideProjects();
+        $("#YoureFired").show();
     });
 });
